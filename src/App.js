178d56ab74618c090f916bcb21b86f8103385616
Dishant <rathidishant39@gmail.com>
2023-06-09 20:49:48 +0530

    Sorry page was missing behind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Login from "./components/login/login";
+import Home from "./components/home/home";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
-    <Login/>
+        <Navbar/>
+    <Home/>
+        {/* <Login/> */}
+            {/* <Login/> */}
+    {/* hieret */}
     </>
   );
 }
